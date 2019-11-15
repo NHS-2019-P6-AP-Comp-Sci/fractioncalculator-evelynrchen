b9254a0c149c0bc2babca0b23091a03dd6297539
s-chenev <s-chenev@53S50030831.it.bsd405.org>
2019-11-15 13:19:26 -0800

    Checkpoint 2

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -9,14 +9,14 @@ import java.util.Scanner;
 public class FracCalc {
 
 	public static void main(String[] args) {
-        // TODO: Read the input from the user and call produceAnswer with an equation
-    	
-    	System.out.println("Please enter your expression."); 
-    	Scanner userInput = new Scanner(System.in); 
-    	String stringInput = userInput.nextLine();
-    	String printOut = produceAnswer(stringInput);
-    	System.out.println(printOut);
-    }
+		// TODO: Read the input from the user and call produceAnswer with an equation
+
+		System.out.println("Please enter your expression.");
+		Scanner userInput = new Scanner(System.in);
+		String stringInput = userInput.nextLine();
+		String printOut = produceAnswer(stringInput);
+		System.out.println(printOut);
+	}
 
 	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
 	// test your code
@@ -32,13 +32,58 @@ public class FracCalc {
 
 	public static String produceAnswer(String Input){
         // TODO: Implement this function to produce the solution to the input
-    	int firstSpace = Input.indexOf(" ");
+		int firstSpace = Input.indexOf(" ");
+		String firstOperand = Input.substring(0, firstSpace);
+
+		int firstUnderscore = firstOperand.indexOf("_");
     	int lastValue = Input.length();
-    	String firstOperand = Input.substring(0, firstSpace);
+    	int firstSlash = firstOperand.indexOf("/");
+    
+    	//first operand first whole number 
+    	String firstWholeNumber = "0";
+    	if (firstUnderscore != -1) {
+    		firstWholeNumber = Input.substring(0, firstUnderscore);
+    	} else {
+    		firstWholeNumber = Input.substring(0, firstSpace);
+    	}
+    
+    	//first numerator and denominator 
+    	String firstNumerator = "0";
+    	String firstDenominator = "0";
+    	if (firstSlash != -1) {
+    		firstNumerator = Input.substring(firstUnderscore + 1, firstSlash);
+        	firstDenominator = Input.substring(firstSlash + 1, firstSpace);
+    	} 
+    	
     	String theOperator = Input.substring(firstSpace + 1);
+    	
     	String secondOperand = Input.substring(firstSpace + 3, lastValue);
-    	return secondOperand;
-    }
+    	
+    	int secondUnderscore = secondOperand.indexOf("_");
+    	int secondSlash = secondOperand.indexOf("/");
+    	int lastSecondValue = secondOperand.length();
+    	
+    	//first whole number 
+    	String secondWholeNumber = "0";
+    	if (secondUnderscore != -1) {
+    		secondWholeNumber = secondOperand.substring(0, secondUnderscore);
+    	} else if (secondSlash == -1) {
+    		secondWholeNumber = secondOperand.substring(0, lastSecondValue);
+    	}
+    	
+    	String secondNumerator = "0"; 
+    	String secondDenominator = "0";
+    	if (secondSlash != -1) {
+    		secondNumerator = secondOperand.substring(secondUnderscore +1, secondSlash);
+    		secondDenominator = secondOperand.substring(secondSlash + 1, lastSecondValue);
+    	} else { 
+    		secondDenominator = "1";
+    	}
+    	
+    	String compiledValue = "whole:" + secondWholeNumber + " numerator:" + secondNumerator + " denominator:" + secondDenominator;
+    	return compiledValue;
+	}
 }
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
+// TODO: Fill in the space below with any helper methods that you think you will
+// need
